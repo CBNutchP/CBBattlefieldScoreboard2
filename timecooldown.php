f725e8029aafd4bdc71bc87e419477e24de4b4ef
thanaponchansuwan <tanapon.chansuwan@gmail.com>
2014-11-13 10:47:49 +0700

    Timecooldown

---
+++ timecooldown.php
@@ -0,0 +1,57 @@
+<head>
+<title></title>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-874">
+</head>
+
+<body bgcolor="#FFFFFF" text="#000000" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<table width="240" border="0" cellspacing="0" cellpadding="0" align="center">
+  <tr>
+    <td width="240" height="240">
+      <div align="center">wait.</span> 
+        <form name="counter">
+  <div align="center">
+    <input type="text" size="5" name="d2" value="">
+  </div>
+</form> 
+
+<div align="center">
+  <script> 
+<!-- 
+// 
+ var milisec=0 
+ var seconds=10 
+ document.counter.d2.value='30' 
+
+function display(){ 
+ if (milisec<=0){ 
+    milisec=9 
+    seconds-=1 
+ } 
+ if (seconds<=-1){ 
+    milisec=0 
+    seconds+=1 
+ } 
+ else 
+    milisec-=1 
+    document.counter.d2.value=seconds+"."+milisec 
+    setTimeout("display()",100) 
+} 
+display() 
+--> 
+</script>
+  <Script Language="JavaScript">
+///ตัวเลข 10000 มีค่าเท่ากับ 10 วินาทีครับ
+function next() 
+{
+window.location="http://test.local/battal.php"
+}
+self.setTimeout('next()', 30000)  //แก้เองนะตรงนี้ให้ตรงกับเวลาข้างบน
+
+</Script>
+</div></div>
+    </td>
+  </tr>
+</table>
+</body>
+</html>
+
