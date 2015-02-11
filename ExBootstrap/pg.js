fbcc738734f9875620a1e813bdb58c6a75ff74a0
suwatuanp <suwat12@gmail.com>
2015-02-11 12:22:22 +0700

    Tester2
    Tester2

--- ExBootstrap/pg.js
+++ ExBootstrap/pg.js
@@ -2,6 +2,9 @@ var sec=60;
 var wsec=2;
 var turn=21;
 var cturn=0;
+var rank;
+var teamwin;
+var p=0;
 var $bar1 = $('#timebar');
 var $bar2 = $('#overallbar');
 var cset;
@@ -12,16 +15,19 @@ function keydown(e){
     var key = e.keyCode;
     //p
     if(key == 80){
-        pgpause();
+        if(p==0){pgstart();
+            p=1;}
+        else{pgpause();
+            p=0;}
     }
     //s
-    if(key == 83){
-        pgstop();
+    if(key == 83){      
+        pgstop();     
     }
     //space
-    if(key == 32){
-        pgstart();
-    }
+    //if(key == 32){
+      //  pgstart();
+    //}
 }
 function pgstart(){
     if(sec==60 || sec==0){
@@ -100,9 +106,29 @@ function progresstime(){
         $bar1.width(sec*(100/60)+"%");
     }
     if ($bar2.width()==0) {
-        clearInterval(progress);
         $bar1.width(0+"%");
-        pgstop();
+        clearInterval(progress);
+        clearTimeout(cset); 
+        if(rank ==0){
+            teamwin ="A";
+        } else if(rank ==1){
+            teamwin ="B";
+        } else if(rank ==2){
+            teamwin ="C";
+        } else if(rank ==3){
+            teamwin ="D";
+        } else if(rank ==4){
+            teamwin ="E";
+        } else if(rank ==5){
+            teamwin ="F";
+        } else if(rank ==6){
+            teamwin ="G";
+        } else if(rank ==7){
+            teamwin ="H";
+        } else{
+            teamwin = "nowin";
+        }
+        alert("winner team : "+ teamwin);
     }
     else{
         $bar2.width(turn*(100/21)+"%");
@@ -110,7 +136,7 @@ function progresstime(){
     $bar2.text(turn+'/21 turn');
     } , 800);
     if(turn==0){
-        alert(rank+"winner");
+        
     }
 }
 function getDataFromDb()
@@ -122,33 +148,28 @@ function getDataFromDb()
       })
       .success(function(result) { 
         var obj = jQuery.parseJSON(result);
-        var rank = 0;
-        var max = -30;
-        document.getElementById("team0").innerHTML="<h2>"+"<font color=\'red\'>" + obj[0]["Total"] + " </font>"+"</h2>";
-        document.getElementById("team1").innerHTML="<h2>"+"<font color=\'red\'>" + obj[1]["Total"] + " </font>"+"</h2>";
-        document.getElementById("team2").innerHTML="<h2>"+"<font color=\'red\'>" + obj[2]["Total"] + " </font>"+"</h2>";
-        document.getElementById("team3").innerHTML="<h2>"+"<font color=\'red\'>" + obj[3]["Total"] + " </font>"+"</h2>";
-        document.getElementById("team4").innerHTML="<h2>"+"<font color=\'red\'>" + obj[4]["Total"] + " </font>"+"</h2>";
-        document.getElementById("team5").innerHTML="<h2>"+"<font color=\'red\'>" + obj[5]["Total"] + " </font>"+"</h2>";
-        document.getElementById("team6").innerHTML="<h2>"+"<font color=\'red\'>" + obj[6]["Total"] + " </font>"+"</h2>";
-        document.getElementById("team7").innerHTML="<h2>"+"<font color=\'red\'>" + obj[7]["Total"] + " </font>"+"</h2>";
-        for(var i=0;i<7;i++){           
-            if(max<obj[i]["Total"]){
-                max=obj[i]["Total"];
-                rank=i;
+        var max = -100;  
+       document.getElementById("team0").innerHTML="<h1>"+"<font color=\'red\'>" + obj[0]["Total"] + " </font>"+"</h1>";
+       document.getElementById("team1").innerHTML="<h1>"+"<font color=\'red\'>" + obj[1]["Total"] + " </font>"+"</h1>";
+       document.getElementById("team2").innerHTML="<h1>"+"<font color=\'red\'>" + obj[2]["Total"] + " </font>"+"</h1>";
+       document.getElementById("team3").innerHTML="<h1>"+"<font color=\'red\'>" + obj[3]["Total"] + " </font>"+"</h1>";
+       document.getElementById("team4").innerHTML="<h1>"+"<font color=\'red\'>" + obj[4]["Total"] + " </font>"+"</h1>";
+       document.getElementById("team5").innerHTML="<h1>"+"<font color=\'red\'>" + obj[5]["Total"] + " </font>"+"</h1>";
+       document.getElementById("team6").innerHTML="<h1>"+"<font color=\'red\'>" + obj[6]["Total"] + " </font>"+"</h1>";
+       document.getElementById("team7").innerHTML="<h1>"+"<font color=\'red\'>" + obj[7]["Total"] + " </font>"+"</h1>";
+        for(var i=0;i<=7;i++){   
+            if(max< parseInt(obj[i]["Total"])){
+                max= parseInt(obj[i]["Total"]);
+                rank=i; 
             }
+        console.log(max);
+        console.log(rank);
         }
-        
-        document.getElementById('team'+rank).innerHTML="<font color=\'blue\'>" + max + " </font>";
-        reGetdata();
-    console.log(max);
+
+        document.getElementById('team'+rank).innerHTML="<h1>"+"<font color=\'blue\'>" + max + " </font>"+"</h1>";
+    
+
     console.log(result);
       });
-}
-function reGetdata(){
-  for(var a=0;a<7;a++){           
-            if(max=obj[a]["Total"]){
-                document.getElementById('team'+rank).innerHTML="<font color=\'blue\'>" + obj[a]["Total"] + " </font>";
-            }
-        }  
+
 }
