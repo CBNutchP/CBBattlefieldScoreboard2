e2eae11bf807f1a1a0439184c1cc1eda51dbf5c8
suwatuanp <suwat12@gmail.com>
2015-02-12 17:02:48 +0700

    dual edit
    dual edit

--- ExBootstrap/edit.php
+++ ExBootstrap/edit.php
@@ -1,66 +1,166 @@
 <?php
 include("connect.php");
 $Turn=$_POST['cturn'];
-$Team=$_POST['cteam'];
-$rTeam;
-$CMD1=$_POST['CMD1'];
-$CMD2=$_POST['CMD2'];
-$KO=$_POST['KO'];
-$Foul=$_POST['PEN'];
-if($Team == 'RED'){
-	$rTeam = 'A';
-}else if($Team == 'GREEN'){
-	$rTeam = 'B';
-}else if($Team == 'YELLOW'){
-	$rTeam = 'C';
+$ATeam=$_POST['ATeam'];
+$ACMD1=$_POST['ACMD1'];
+$ACMD2=$_POST['ACMD2'];
+$AKO=$_POST['AKO'];
+$AFoul=$_POST['APEN'];
+if($ACMD1 =='A' && $ACMD2=='A'){
+	$APIT = 3 + $AFoul;
 }
-else if($Team == 'BLUE'){
-	$rTeam = 'D';
-}else if($Team == 'CYAN'){
-	$rTeam = 'E';
-}else if($Team == 'PINK'){
-	$rTeam = 'F';
-}else if($Team == 'PURPLE'){
-	$rTeam = 'G';
-}else if($Team == 'ORANGE'){
-	$rTeam = 'H';
-}
-if($CMD1 =='A' && $CMD2=='A'){
-	$PIT = 3 + $Foul;
-}
-else if($CMD1 =='A' || $CMD2=='A'){
-	$PIT = 1 + $Foul;
+else if($ACMD1 =='A' || $ACMD2=='A'){
+	$APIT = 1 + $AFoul;
 }
 else{
-	$PIT = 0 + $Foul;
+	$APIT = 0 + $AFoul;
 }
-if($KO == '1'){
-	$PIT =$PIT-5 + $Foul;
+if($AKO == '1'){
+	$APIT =$APIT-5 + $AFoul;
 }
-mysql_query( "UPDATE TurnTb SET Commander1='$CMD1',Commander2='$CMD2',PIT='$PIT' WHERE Turn='$Turn' AND Team='$rTeam' ");
-mysql_query( "UPDATE PenaltyTb SET Foul='$AFoul'  WHERE Turn='$Turn' AND Team='$Team'");
-mysql_query( "UPDATE KO SET KO='$KO' WHERE Turn='$Turn' AND Team='$Team' ");
-// mysql_query( "UPDATE TurnTb SET Commander1='$BCMD1',Commander2='$BCMD2',PIT='$BPIT' WHERE Turn='$Turn' AND Team='$BTeam' ");
-// mysql_query( "UPDATE PenaltyTb SET Foul='$BFoul'  WHERE Turn='$Turn' AND Team='$BTeam'");
-// mysql_query( "UPDATE KO SET KO='$BKO' WHERE Turn='$Turn' AND Team='$BTeam' ");
-// mysql_query( "UPDATE TurnTb SET Commander1='$CCMD1',Commander2='$CCMD2',PIT='$CPIT' WHERE Turn='$Turn' AND Team='$CTeam' ");
-// mysql_query( "UPDATE PenaltyTb SET Foul='$AFoul'  WHERE Turn='$Turn' AND Team='$ATeam'");
-// mysql_query( "UPDATE KO SET KO='$AKO' WHERE Turn='$Turn' AND Team='$ATeam' ");
-// mysql_query( "UPDATE TurnTb SET Commander1='$DCMD1',Commander2='$DCMD2',PIT='$DPIT' WHERE Turn='$Turn' AND Team='$DTeam' ");
-// mysql_query( "UPDATE PenaltyTb SET Foul='$DFoul'  WHERE Turn='$Turn' AND Team='$DTeam'");
-// mysql_query( "UPDATE KO SET KO='$DKO' WHERE Turn='$Turn' AND Team='$DTeam' ");
-// mysql_query( "UPDATE TurnTb SET Commander1='$ECMD1',Commander2='$ECMD2',PIT='$EPIT' WHERE Turn='$Turn' AND Team='$ETeam' ");
-// mysql_query( "UPDATE PenaltyTb SET Foul='$EFoul'  WHERE Turn='$Turn' AND Team='$ETeam'");
-// mysql_query( "UPDATE KO SET KO='$EKO' WHERE Turn='$Turn' AND Team='$ETeam' ");
-// mysql_query( "UPDATE TurnTb SET Commander1='$FCMD1',Commander2='$FCMD2',PIT='$BPIT' WHERE Turn='$Turn' AND Team='$FTeam' ");
-// mysql_query( "UPDATE PenaltyTb SET Foul='$FFoul'  WHERE Turn='$Turn' AND Team='$FTeam'");
-// mysql_query( "UPDATE KO SET KO='$FKO' WHERE Turn='$Turn' AND Team='$FTeam' ");
-// mysql_query( "UPDATE TurnTb SET Commander1='$GCMD1',Commander2='$GCMD2',PIT='$GPIT' WHERE Turn='$Turn' AND Team='$GTeam' ");
-// mysql_query( "UPDATE PenaltyTb SET Foul='$GFoul'  WHERE Turn='$Turn' AND Team='$GTeam'");
-// mysql_query( "UPDATE KO SET KO='$GKO' WHERE Turn='$Turn' AND Team='$GTeam' ");
-// mysql_query( "UPDATE TurnTb SET Commander1='$HCMD1',Commander2='$HCMD2',PIT='$HPIT' WHERE Turn='$Turn' AND Team='$HTeam' ");
-// mysql_query( "UPDATE PenaltyTb SET Foul='$HFoul'  WHERE Turn='$Turn' AND Team='$HTeam'");
-// mysql_query( "UPDATE KO SET KO='$HKO' WHERE Turn='$Turn' AND Team='$HTeam' ");
+$BTeam=$_POST['BTeam'];
+$BCMD1=$_POST['BCMD1'];
+$BCMD2=$_POST['BCMD2'];
+$BKO=$_POST['BKO'];
+$BFoul=$_POST['BPEN'];
+if($BCMD1 =='A' && $BCMD2=='A'){
+	$BPIT = 3 + $BFoul;
+}
+else if($ACMD1 =='A' || $ACMD2=='A'){
+	$BPIT = 1 + $BFoul;
+}
+else{
+	$BPIT = 0 + $BFoul;
+}
+if($AKO == '1'){
+	$BPIT =$BPIT-5 + $BFoul;
+}
+$CTeam=$_POST['CTeam'];
+$CCMD1=$_POST['CCMD1'];
+$CCMD2=$_POST['CCMD2'];
+$CKO=$_POST['CKO'];
+$CFoul=$_POST['CPEN'];
+if($CCMD1 =='A' && $CCMD2=='A'){
+	$CPIT = 3 + $CFoul;
+}
+else if($ACMD1 =='A' || $ACMD2=='A'){
+	$CPIT = 1 + $CFoul;
+}
+else{
+	$CPIT = 0 + $CFoul;
+}
+if($CKO == '1'){
+	$CPIT =$CPIT-5 + $CFoul;
+}
+$DTeam=$_POST['DTeam'];
+$DCMD1=$_POST['DCMD1'];
+$DCMD2=$_POST['DCMD2'];
+$DKO=$_POST['DKO'];
+$DFoul=$_POST['DPEN'];
+if($DCMD1 =='A' && $DCMD2=='A'){
+	$APIT = 3 + $AFoul;
+}
+else if($DCMD1 =='A' || $DCMD2=='A'){
+	$DPIT = 1 + $DFoul;
+}
+else{
+	$DPIT = 0 + $DFoul;
+}
+if($DKO == '1'){
+	$DPIT =$DPIT-5 + $DFoul;
+}
+$ETeam=$_POST['ETeam'];
+$ECMD1=$_POST['ECMD1'];
+$ECMD2=$_POST['ECMD2'];
+$EKO=$_POST['EKO'];
+$EFoul=$_POST['EPEN'];
+if($ECMD1 =='A' && $ECMD2=='A'){
+	$EPIT = 3 + $EFoul;
+}
+else if($ECMD1 =='A' || $ECMD2=='A'){
+	$EPIT = 1 + $EFoul;
+}
+else{
+	$EPIT = 0 + $EFoul;
+}
+if($EKO == '1'){
+	$EPIT =$EPIT-5 + $EFoul;
+}
+$FTeam=$_POST['FTeam'];
+$FCMD1=$_POST['FCMD1'];
+$FCMD2=$_POST['FCMD2'];
+$FKO=$_POST['FKO'];
+$FFoul=$_POST['FPEN'];
+if($FCMD1 =='A' && $FCMD2=='A'){
+	$FPIT = 3 + $FFoul;
+}
+else if($FCMD1 =='A' || $FCMD2=='A'){
+	$FPIT = 1 + $FFoul;
+}
+else{
+	$FPIT = 0 + $FFoul;
+}
+if($FKO == '1'){
+	$FPIT =$FPIT-5 + $FFoul;
+}
+$GTeam=$_POST['GTeam'];
+$GCMD1=$_POST['GCMD1'];
+$GCMD2=$_POST['GCMD2'];
+$GKO=$_POST['GKO'];
+$GFoul=$_POST['CPEN'];
+if($GCMD1 =='A' && $GCMD2=='A'){
+	$GPIT = 3 + $GFoul;
+}
+else if($GCMD1 =='A' || $GCMD2=='A'){
+	$GPIT = 1 + $GFoul;
+}
+else{
+	$GPIT = 0 + $GFoul;
+}
+if($GKO == '1'){
+	$GPIT =$GPIT-5 + $GFoul;
+}
+$HTeam=$_POST['HTeam'];
+$HCMD1=$_POST['HCMD1'];
+$HCMD2=$_POST['HCMD2'];
+$HKO=$_POST['HKO'];
+$HFoul=$_POST['HPEN'];
+if($HCMD1 =='A' && $HCMD2=='A'){
+	$APIT = 3 + $AFoul;
+}
+else if($HCMD1 =='A' || $HCMD2=='A'){
+	$HPIT = 1 + $HFoul;
+}
+else{
+	$HPIT = 0 + $HFoul;
+}
+if($HKO == '1'){
+	$HPIT =$HPIT-5 + $HFoul;
+}
+mysql_query( "UPDATE TurnTb SET Commander1='$ACMD1',Commander2='$ACMD2',PIT='$APIT' WHERE Turn='$Turn' AND Team='$ATeam' ");
+mysql_query( "UPDATE PenaltyTb SET Foul='$AFoul'  WHERE Turn='$Turn' AND Team='$ATeam'");
+mysql_query( "UPDATE KO SET KO='$AKO' WHERE Turn='$Turn' AND Team='$ATeam' ");
+mysql_query( "UPDATE TurnTb SET Commander1='$BCMD1',Commander2='$BCMD2',PIT='$BPIT' WHERE Turn='$Turn' AND Team='$BTeam' ");
+mysql_query( "UPDATE PenaltyTb SET Foul='$BFoul'  WHERE Turn='$Turn' AND Team='$BTeam'");
+mysql_query( "UPDATE KO SET KO='$BKO' WHERE Turn='$Turn' AND Team='$BTeam' ");
+mysql_query( "UPDATE TurnTb SET Commander1='$CCMD1',Commander2='$CCMD2',PIT='$CPIT' WHERE Turn='$Turn' AND Team='$CTeam' ");
+mysql_query( "UPDATE PenaltyTb SET Foul='$AFoul'  WHERE Turn='$Turn' AND Team='$ATeam'");
+mysql_query( "UPDATE KO SET KO='$AKO' WHERE Turn='$Turn' AND Team='$ATeam' ");
+mysql_query( "UPDATE TurnTb SET Commander1='$DCMD1',Commander2='$DCMD2',PIT='$DPIT' WHERE Turn='$Turn' AND Team='$DTeam' ");
+mysql_query( "UPDATE PenaltyTb SET Foul='$DFoul'  WHERE Turn='$Turn' AND Team='$DTeam'");
+mysql_query( "UPDATE KO SET KO='$DKO' WHERE Turn='$Turn' AND Team='$DTeam' ");
+mysql_query( "UPDATE TurnTb SET Commander1='$ECMD1',Commander2='$ECMD2',PIT='$EPIT' WHERE Turn='$Turn' AND Team='$ETeam' ");
+mysql_query( "UPDATE PenaltyTb SET Foul='$EFoul'  WHERE Turn='$Turn' AND Team='$ETeam'");
+mysql_query( "UPDATE KO SET KO='$EKO' WHERE Turn='$Turn' AND Team='$ETeam' ");
+mysql_query( "UPDATE TurnTb SET Commander1='$FCMD1',Commander2='$FCMD2',PIT='$BPIT' WHERE Turn='$Turn' AND Team='$FTeam' ");
+mysql_query( "UPDATE PenaltyTb SET Foul='$FFoul'  WHERE Turn='$Turn' AND Team='$FTeam'");
+mysql_query( "UPDATE KO SET KO='$FKO' WHERE Turn='$Turn' AND Team='$FTeam' ");
+mysql_query( "UPDATE TurnTb SET Commander1='$GCMD1',Commander2='$GCMD2',PIT='$GPIT' WHERE Turn='$Turn' AND Team='$GTeam' ");
+mysql_query( "UPDATE PenaltyTb SET Foul='$GFoul'  WHERE Turn='$Turn' AND Team='$GTeam'");
+mysql_query( "UPDATE KO SET KO='$GKO' WHERE Turn='$Turn' AND Team='$GTeam' ");
+mysql_query( "UPDATE TurnTb SET Commander1='$HCMD1',Commander2='$HCMD2',PIT='$HPIT' WHERE Turn='$Turn' AND Team='$HTeam' ");
+mysql_query( "UPDATE PenaltyTb SET Foul='$HFoul'  WHERE Turn='$Turn' AND Team='$HTeam'");
+mysql_query( "UPDATE KO SET KO='$HKO' WHERE Turn='$Turn' AND Team='$HTeam' ");
 echo "แก้แต้มแล้ว";
 header("Location: Edit.html"); 
 ?>
--- ExBootstrap/edit2.php
+++ ExBootstrap/edit2.php
@@ -1,16 +1,66 @@
-<?
-$hostname="localhost";
-$username="root";
-$password="mysql";
-$database="company";
-mysql_connect($hostname,$username,$password) or die( "Unable to connect database");
-mysql_select_db($database) or die( "Unable to select database");
-$query="CREATE TABLE employee
-(id int(6) NOT NULL auto_increment,
-first varchar(15) NOT NULL,
-last varchar(15) NOT NULL,
-phone varchar(20) NOT NULL,
-email varchar(30) NOT NULL,
-web varchar(30) NOT NULL,
-PRIMARY KEY (id))";mysql_query($query);mysql_close();
-echo "สร้างตาราง employee เรียบร้อยแล้ว";
+<?php
+include("connect.php");
+$Turn=$_POST['cturn'];
+$Team=$_POST['cteam'];
+$rTeam;
+$CMD1=$_POST['CMD1'];
+$CMD2=$_POST['CMD2'];
+$KO=$_POST['KO'];
+$Foul=$_POST['PEN'];
+if($Team == 'RED'){
+	$rTeam = 'A';
+}else if($Team == 'GREEN'){
+	$rTeam = 'B';
+}else if($Team == 'YELLOW'){
+	$rTeam = 'C';
+}
+else if($Team == 'BLUE'){
+	$rTeam = 'D';
+}else if($Team == 'CYAN'){
+	$rTeam = 'E';
+}else if($Team == 'PINK'){
+	$rTeam = 'F';
+}else if($Team == 'PURPLE'){
+	$rTeam = 'G';
+}else if($Team == 'ORANGE'){
+	$rTeam = 'H';
+}
+if($CMD1 =='A' && $CMD2=='A'){
+	$PIT = 3 + $Foul;
+}
+else if($CMD1 =='A' || $CMD2=='A'){
+	$PIT = 1 + $Foul;
+}
+else{
+	$PIT = 0 + $Foul;
+}
+if($KO == '1'){
+	$PIT =$PIT-5 + $Foul;
+}
+mysql_query( "UPDATE TurnTb SET Commander1='$CMD1',Commander2='$CMD2',PIT='$PIT' WHERE Turn='$Turn' AND Team='$rTeam' ");
+mysql_query( "UPDATE PenaltyTb SET Foul='$AFoul'  WHERE Turn='$Turn' AND Team='$Team'");
+mysql_query( "UPDATE KO SET KO='$KO' WHERE Turn='$Turn' AND Team='$Team' ");
+// mysql_query( "UPDATE TurnTb SET Commander1='$BCMD1',Commander2='$BCMD2',PIT='$BPIT' WHERE Turn='$Turn' AND Team='$BTeam' ");
+// mysql_query( "UPDATE PenaltyTb SET Foul='$BFoul'  WHERE Turn='$Turn' AND Team='$BTeam'");
+// mysql_query( "UPDATE KO SET KO='$BKO' WHERE Turn='$Turn' AND Team='$BTeam' ");
+// mysql_query( "UPDATE TurnTb SET Commander1='$CCMD1',Commander2='$CCMD2',PIT='$CPIT' WHERE Turn='$Turn' AND Team='$CTeam' ");
+// mysql_query( "UPDATE PenaltyTb SET Foul='$AFoul'  WHERE Turn='$Turn' AND Team='$ATeam'");
+// mysql_query( "UPDATE KO SET KO='$AKO' WHERE Turn='$Turn' AND Team='$ATeam' ");
+// mysql_query( "UPDATE TurnTb SET Commander1='$DCMD1',Commander2='$DCMD2',PIT='$DPIT' WHERE Turn='$Turn' AND Team='$DTeam' ");
+// mysql_query( "UPDATE PenaltyTb SET Foul='$DFoul'  WHERE Turn='$Turn' AND Team='$DTeam'");
+// mysql_query( "UPDATE KO SET KO='$DKO' WHERE Turn='$Turn' AND Team='$DTeam' ");
+// mysql_query( "UPDATE TurnTb SET Commander1='$ECMD1',Commander2='$ECMD2',PIT='$EPIT' WHERE Turn='$Turn' AND Team='$ETeam' ");
+// mysql_query( "UPDATE PenaltyTb SET Foul='$EFoul'  WHERE Turn='$Turn' AND Team='$ETeam'");
+// mysql_query( "UPDATE KO SET KO='$EKO' WHERE Turn='$Turn' AND Team='$ETeam' ");
+// mysql_query( "UPDATE TurnTb SET Commander1='$FCMD1',Commander2='$FCMD2',PIT='$BPIT' WHERE Turn='$Turn' AND Team='$FTeam' ");
+// mysql_query( "UPDATE PenaltyTb SET Foul='$FFoul'  WHERE Turn='$Turn' AND Team='$FTeam'");
+// mysql_query( "UPDATE KO SET KO='$FKO' WHERE Turn='$Turn' AND Team='$FTeam' ");
+// mysql_query( "UPDATE TurnTb SET Commander1='$GCMD1',Commander2='$GCMD2',PIT='$GPIT' WHERE Turn='$Turn' AND Team='$GTeam' ");
+// mysql_query( "UPDATE PenaltyTb SET Foul='$GFoul'  WHERE Turn='$Turn' AND Team='$GTeam'");
+// mysql_query( "UPDATE KO SET KO='$GKO' WHERE Turn='$Turn' AND Team='$GTeam' ");
+// mysql_query( "UPDATE TurnTb SET Commander1='$HCMD1',Commander2='$HCMD2',PIT='$HPIT' WHERE Turn='$Turn' AND Team='$HTeam' ");
+// mysql_query( "UPDATE PenaltyTb SET Foul='$HFoul'  WHERE Turn='$Turn' AND Team='$HTeam'");
+// mysql_query( "UPDATE KO SET KO='$HKO' WHERE Turn='$Turn' AND Team='$HTeam' ");
+echo "แก้แต้มแล้ว";
+header("Location: Edit.html"); 
+?>
